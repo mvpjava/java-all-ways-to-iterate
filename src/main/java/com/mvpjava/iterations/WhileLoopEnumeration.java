b0077be29891d297f7f1e83b68833685dbfe6666
Andy Luis <info@triomni-it.com>
2021-09-02 18:52:18 +0000

    Initial Commit

---
+++ src/main/java/com/mvpjava/iterations/WhileLoopEnumeration.java
@@ -0,0 +1,46 @@
+package com.mvpjava.iterations;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+import static java.util.List.*;
+
+public class WhileLoopEnumeration {
+    private static final List <String> famousPeople ;
+
+    static  {
+        famousPeople = of("Napoleon", "William Shakespeare", "Aristotle", "Alexander the Great", "Charles Darwin");
+    }
+
+    public static void loop(){
+        System.out.println("================== While loop");
+        System.out.println("================== While loop ... Iterator");
+
+        // Abstract iteration details of underlying Collection which could be a Map (via EntrySet), Set or List
+        // and not invloved in buggish code with indexes etc ..
+        Iterator<String> iterator = famousPeople.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("================== While loop ... Java 8 forEachRemaining - NO LOOP Necessary!!");
+        // No while loop and no methods off the iterator to call. Love it!
+        famousPeople.iterator().forEachRemaining( (person) -> {
+            System.out.println(person);
+        });
+
+        // OR
+        System.out.println("================== While loop ... List index counter");
+        // have to manage the extra local variable, correct logic operator and local var incrementation
+        // which are all potential sources of bugs as well as your aware that you are looping through a list
+        // because your executing the .size and .get methods.
+        // only useful if you really need the index
+
+        int index = 0;
+        while ( index < famousPeople.size()){
+            System.out.println(famousPeople.get(index));
+            index++;
+        }
+    }
+
+}
---
+++ src/test/java/com/mvpjava/iterations/JavaAllWaysToIterateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvpjava.iterations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAllWaysToIterateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
